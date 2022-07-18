4dac2abcb2b8943f24ea525b6c3ce87362029d07
binginx <binginx@binginx.local>
2022-07-18 17:15:25 +0800

    链路追踪

--- sleuth-a-service/src/main/java/com/sdjictec/scalidemo/sleutha/service/SleuthbServiceFeignClient.java
+++ sleuth-a-service/src/main/java/com/sdjictec/scalidemo/sleutha/service/SleuthbServiceFeignClient.java
@@ -3,7 +3,7 @@ package com.sdjictec.scalidemo.sleutha.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("sleuth-b-service")
+@FeignClient("sleuthb-service")
 public interface SleuthbServiceFeignClient {
     @GetMapping("/sleuthb")
     public String methodB();
--- sleuth-b-service/src/main/java/com/sdjictec/scalidemo/sleuthb/service/SleuthcServiceFeignClient.java
+++ sleuth-b-service/src/main/java/com/sdjictec/scalidemo/sleuthb/service/SleuthcServiceFeignClient.java
@@ -3,7 +3,7 @@ package com.sdjictec.scalidemo.sleuthb.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("sleuth-c-service")
+@FeignClient("sleuthc-service")
 public interface SleuthcServiceFeignClient {
     @GetMapping("/sleuthc")
     public String methodC();
